796c4cb5c32c136985635a3a8f6ea58d0e4232dd
BastienDGit <130741134+BastienDGit@users.noreply.github.com>
2024-11-23 17:48:12 +0100

    Update halloffame.php

--- core-master/views/halloffame.php
+++ core-master/views/halloffame.php
@@ -10,5 +10,29 @@
     <h1>Hall of fame<h1>
     <a href = "/escapegame">Jouer</a>
     <a href = "/accueil">Ecran titre</a>
+        <h1>TOP 10 DES MEILLEURS JOUEURS</h1>
+    <table id="playertable">
+    
+    <table>
+        <tr>
+            <th>PSEUDO</th> 
+            <th>TEMPS</th>
+            <th>SCORE</th>
+            <th>RANG</th>
+        </tr>
+
+        <?php
+        $rang = 1;
+        foreach ($data as $row) {
+            echo "<tr>";
+            echo "<td>" .$row['pseudo'] . "</td>";
+            echo "<td>" .$row['temps'] . "</td>";
+            echo "<td>" .$row['score'] . "</td>";
+            echo "<td>" .$rang . "</td>";
+            echo "</tr>";
+            $rang++;
+        }
+        ?>
+    </table>
 </body>
 </html>
